d589d43872c638227aa6b108a1bb735753b24ca2
Nguyen Minh An <anmin.nguyen608@gmail.com>
2022-03-24 20:57:15 +0700

    test

--- client-side/src/pages/AddNewProduct.js
+++ client-side/src/pages/AddNewProduct.js
@@ -3,6 +3,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import axios from "axios";
 import * as Yup from "yup";
 import "../styles/add-product.css";
+
 function AddNewProduct() {
   const initialValues = {
     name: "",
@@ -26,6 +27,8 @@ function AddNewProduct() {
       console.log("Product has been uploaded");
     });
   };
+
+  
   return (
     <div className="create-product-page">
       <Formik
@@ -34,32 +37,33 @@ function AddNewProduct() {
         validation={validationSchema}
       >
         <Form>
-          <label>Name:</label>
+          <label htmlFor="name">Name:</label>
           <ErrorMessage name="name" component="span" />
           <Field id="inputCreatePost" name="name"></Field>
-          <label>Category:</label>
-          <ErrorMessage name="Category" component="span" />
+          <label htmlFor="category">Category:</label>
+          <ErrorMessage name="category" component="span" />
           <Field id="inputCreatePost" name="category"></Field>
-          <label>Price:</label>
+          <label htmlFor="price">Price:</label>
           <ErrorMessage name="price" component="span" />
           <Field id="inputCreatePost" name="price"></Field>
-          <label>Sizes:</label>
+          <label htmlFor="size">Sizes:</label>
           <ErrorMessage name="sizes" component="span" />
           <Field id="inputCreatePost" name="sizes"></Field>
-          <label>Colors:</label>
+          <label htmlFor="colors">Colors:</label>
           <ErrorMessage name="colors" component="span" />
           <Field id="inputCreatePost" name="colors"></Field>
-          <label>PictSource:</label>
+          <label htmlFor="pictSource">PictSource:</label>
           <ErrorMessage name="pictSource" component="span" />
           <Field id="inputCreatePost" name="pictSource"></Field>
-          <label>Description:</label>
-          <ErrorMessage name="pictSource" component="span" />
+          <label htmlFor="description">Description:</label>
+          <ErrorMessage name="description" component="span" />
           <Field id="inputCreatePost" name="description" as="textarea"></Field>
           <button type="submit">Create Product</button>
         </Form>
       </Formik>
     </div>
   );
+
 }
 
 export default AddNewProduct;
