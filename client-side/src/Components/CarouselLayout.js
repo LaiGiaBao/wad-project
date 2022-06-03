5885a018a96faf70197f31881f642db8936f8919
Chillisaucery <66952546+Chillisaucery@users.noreply.github.com>
2022-06-03 22:50:24 +0700

    Added 3 images in the Carousel

--- client-side/src/Components/CarouselLayout.js
+++ client-side/src/Components/CarouselLayout.js
@@ -7,7 +7,7 @@ const CarouselLayout = (children) => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={require("../img/carousels/1-color.png")}
+          src={"https://media.istockphoto.com/photos/elegant-black-leather-shoes-picture-id172417586?k=20&m=172417586&s=612x612&w=0&h=DDjvQhRgSYcH2F5rVt8iohGvkqCIteYuTCq3wpJuUi4="}
           alt="First slide"
         />
         <Carousel.Caption>
@@ -18,7 +18,7 @@ const CarouselLayout = (children) => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={require("../img/carousels/1-color.png")}
+          src={"https://images.squarespace-cdn.com/content/v1/52d654d2e4b0a3af71bf6bcc/1627659581969-5EA7H241G2YETQFQFGF9/SIF+1948+white+dial?format=1000w"}
           alt="Second slide"
         />
         <Carousel.Caption>
@@ -29,7 +29,7 @@ const CarouselLayout = (children) => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={require("../img/carousels/1-color.png")}
+          src={"https://img.freepik.com/free-photo/men-s-clothing-set-with-oxford-shoes-watch-sunglasses-office-shirt-tie-jacket-isolated-white-background-top-view_107612-73.jpg"}
           alt="Third slide"
         />
         <Carousel.Caption>
